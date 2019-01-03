7302e6e9c17d241b6c61e4e45d896fece503744b
liaop <7192506@qq.com>
2019-01-03 15:08:14 +0800

    守护进程在崩溃后重启服务

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <cstring>
 #include "MainServer.h"
 
 #define SERVER_DEBUG
@@ -17,12 +19,25 @@
 
 #endif
 
-int main(){
+int main(int argc, char* argv[]){
+    char path[128] = {0};
+    char propert[128] = {0};
+    if (argc == 2){
+        strcpy(path, argv[1]);
+        strcpy(propert, path);
+        strcat(propert, "/log.properties");
+    }else{
+        getcwd(path, 128);
+        strcpy(propert, path);
+        strcat(propert, "/log.properties");
+    }
+    printf("path=%s, pro=%s\n", path, propert);
 #ifndef SERVER_DEBUG
     daemon(0, 0);
+    chdir(path);
 #endif
     CLogger *logger = nullptr;
-    logger = new CLogger("SettServer", "log.properties");
+    logger = new CLogger("SettServer", propert);
     while(true){
         pid_t pid = fork();
         if (pid > 0){
@@ -43,38 +58,39 @@ int main(){
     }
 }
 
+/*
+int main() {
+    CLogger *logger = nullptr;
+    logger = new CLogger("SettServer", "log.properties");
 
-//int main01() {
-//    CLogger *logger = nullptr;
-//    logger = new CLogger("SettServer", "log.properties");
-//
-//    MainServer *server = nullptr;
-//    server = new MainServer(KAFKA_HOSTS, FRONT_ADDR, logger);
-//#ifndef SERVER_DEBUG
-//    daemon(0, 0);
-//#endif
-//
-//    int ret = server->init();
-//    if (ret != 0){
-//        delete(server);
-//        delete(logger);
-//        exit(-1);
-//    }
-//
-//    while(!ret){
-//        try{
-//            server->run();
-//            server->destory();
-//            ret = server->init();
-//        }catch (...){
-//            logger->error("主服务器出现异常退出，1秒后重新启动");
-//            server->destory();
-//            sleep(1);
-//            ret = server->init();
-//        }
-//    }
-//    server->destory();
-//    delete(server);
-//    delete(logger);
-//    return 0;
-//}
+    MainServer *server = nullptr;
+    server = new MainServer(KAFKA_HOSTS, FRONT_ADDR, logger);
+#ifndef SERVER_DEBUG
+    daemon(0, 0);
+#endif
+
+    int ret = server->init();
+    if (ret != 0){
+        delete(server);
+        delete(logger);
+        exit(-1);
+    }
+
+    while(!ret){
+        try{
+            server->run();
+            server->destory();
+            ret = server->init();
+        }catch (...){
+            logger->error("主服务器出现异常退出，1秒后重新启动");
+            server->destory();
+            sleep(1);
+            ret = server->init();
+        }
+    }
+    server->destory();
+    delete(server);
+    delete(logger);
+    return 0;
+}
+*/
